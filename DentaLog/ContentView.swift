296fd114852d0e8a0e3295ef72ecbac7f11c612f
Abdulloh <abdullohbahromjonv@gmail.com>
2025-01-09 22:30:30 +0500

    Initial Commit

---
+++ DentaLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DentaLog
+//
+//  Created by Abdulloh Bahromjonov on 09/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DentaLog/DentaLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DentaLogApp.swift
+//  DentaLog
+//
+//  Created by Abdulloh Bahromjonov on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DentaLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
